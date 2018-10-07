b9478e753cf9d698d092b6b9a74f463504ff0f22
JackYumCha <jackdatascience@gmail.com>
2018-10-07 14:07:42 +1000

    setup

---
+++ Jack.DataScience/Jack.DataScience.DataTypes/DemoEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using Jack.DataScience.Data.NpgSQL;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jack.DataScience.DataTypes
+{
+
+    class DemoEntity
+    {
+        [BsonId, Key, Required]
+        public string _id { get; set; }
+        [Index, StringLength(255)]
+        public string Name { get; set; }
+        [Index]
+        public int Age { get; set; }
+        [Index, StringLength(255)]
+        public string Email { get; set; }
+    }
+}
