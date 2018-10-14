45a0872cb03716875496640831910e8c3ded079e
Zhenyu Shi <errisy@gmail.com>
2018-10-14 18:03:23 +1000

    sftp download

--- Jack.DataScience/Jack.DataScience.Storage.SFTP.Tests/SFTPTests.cs
+++ Jack.DataScience/Jack.DataScience.Storage.SFTP.Tests/SFTPTests.cs
@@ -30,6 +30,7 @@ namespace Jack.DataScience.Storage.SFTP.Tests
 
         [Theory(DisplayName = "Upload String as File")]
         [InlineData("this is a text file")]
+        [InlineData("this is another text file")]
         public void UploadStringAsFile(string content)
         {
             AutoFacContainer container = new AutoFacContainer();
@@ -41,7 +42,7 @@ namespace Jack.DataScience.Storage.SFTP.Tests
                 client.Connect();
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
                 {
-                    client.UploadFile(stream, "./test.txt");
+                    client.UploadFile(stream, "./test2.txt");
                     Debugger.Break();
                 }
                 client.Disconnect();
@@ -50,6 +51,7 @@ namespace Jack.DataScience.Storage.SFTP.Tests
 
         [Theory(DisplayName = "Delete a File")]
         [InlineData("./test.txt")]
+        [InlineData("./test2.txt")]
         public void DeleteAFile(string filename)
         {
             AutoFacContainer container = new AutoFacContainer();
@@ -61,6 +63,31 @@ namespace Jack.DataScience.Storage.SFTP.Tests
                 client.Connect();
                 client.DeleteFile(filename);
                 Debugger.Break();
+                client.Disconnect();
+            }
+        }
+
+        [Fact(DisplayName = "Download a File")]
+        public void DownloadFile()
+        {
+            AutoFacContainer container = new AutoFacContainer();
+            container.RegisterOptions<SshOptions>();
+            container.ContainerBuilder.RegisterModule<SshModule>();
+            var servicesContainer = container.ContainerBuilder.Build();
+            using (var client = servicesContainer.Resolve<SftpClient>())
+            {
+                client.Connect();
+                using(FileStream fs = new FileStream($"{AppContext.BaseDirectory}/downloaded.zip", FileMode.OpenOrCreate))
+                {
+                    client.DownloadFile("./4117.zip", fs);
+                }
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    client.DownloadFile("./test2.txt", ms);
+                    var text = Encoding.UTF8.GetString(ms.ToArray());
+                    Debugger.Break();
+                }
                 client.Disconnect();
             }
         }
