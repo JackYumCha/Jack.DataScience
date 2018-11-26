18def9a549cb489a6928dbed0195958fd5f98786
Jack Yum Cha <jackdatascience@gmail.com>
2018-11-26 13:53:31 +1000

    athena project

---
+++ Jack.DataScience/Jack.DataScience.Data.AWSAthena/Athena.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.Athena;
+using Amazon.Athena.Model;
+
+namespace Jack.DataScience.Data.AWSAthena
+{
+    public class Athena
+    {
+        private readonly AmazonAthenaClient amazonAthenaClient;
+        public Athena(AmazonAthenaClient amazonAthenaClient)
+        {
+            this.amazonAthenaClient = amazonAthenaClient;
+        }
+
+        public void Test()
+        {
+            amazonAthenaClient.StartQueryExecutionAsync(new StartQueryExecutionRequest()
+            {
+                QueryString = "",
+                QueryExecutionContext = new QueryExecutionContext()
+                {
+                },
+                ClientRequestToken = "",
+                ResultConfiguration = new ResultConfiguration
+                {
+                    EncryptionConfiguration = new EncryptionConfiguration
+                    {
+                        EncryptionOption = EncryptionOption.SSE_S3
+                    },
+                    OutputLocation = "s3;//abcd.ef"
+                }
+            });
+
+            amazonAthenaClient.CreateNamedQueryAsync
+        }
+    }
+}
---
+++ Jack.DataScience/Jack.DataScience.Data.AWSAthena/AthenaModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Amazon.Athena;
+using Amazon.Athena.Model;
+using Amazon;
+using Autofac;
+
+namespace Jack.DataScience.Data.AWSAthena
+{
+    public class AthenaModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+            base.Load(builder);
+        }
+    }
+}
