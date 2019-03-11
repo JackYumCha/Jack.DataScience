0fd1aeb0300b00c1b407aec1af060b357d503d60
Jack Yum Cha <jackdatascience@gmail.com>
2019-03-11 11:35:25 +1000

    change mongo

--- Jack.DataScience/Jack.DataScience.Data.MongoDB/MongoModule.cs
+++ Jack.DataScience/Jack.DataScience.Data.MongoDB/MongoModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Autofac;
 using MongoDB.Driver;
+using System.Security.Authentication;
 using Jack.DataScience.Common;
 
 namespace Jack.DataScience.Data.MongoDB
@@ -15,7 +16,14 @@ namespace Jack.DataScience.Data.MongoDB
             builder.Register((context) =>
             {
                 var options = context.Resolve<MongoOptions>();
-                return new MongoClient(options.Url);
+
+                MongoClientSettings mongoClientSettings = MongoClientSettings.FromUrl(new MongoUrl(options.Url));
+
+                mongoClientSettings.SslSettings = new SslSettings()
+                {
+                    EnabledSslProtocols = SslProtocols.Tls12
+                };
+                return new MongoClient(mongoClientSettings);
             });
             base.Load(builder);
         }
