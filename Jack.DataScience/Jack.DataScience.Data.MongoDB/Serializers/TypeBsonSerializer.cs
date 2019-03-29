f8398ee29bb82be9b510ee8b5d2ff08a752c0897
Zhenyu Shi <errisy@gmail.com>
2019-03-30 04:12:24 +1000

    fix serializer

--- Jack.DataScience/Jack.DataScience.Data.MongoDB/Serializers/TypeBsonSerializer.cs
+++ Jack.DataScience/Jack.DataScience.Data.MongoDB/Serializers/TypeBsonSerializer.cs
@@ -1,27 +1,57 @@
-﻿using MongoDB.Bson.Serialization;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Jack.DataScience.Data.MongoDB.Serializers
 {
     public class TypeBsonSerializer : SerializerBase<Type>
     {
+        private static bool IsRegistered = false;
+        public static void Register()
+        {
+            if (!Volatile.Read(ref IsRegistered))
+            {
+                Volatile.Write(ref IsRegistered, true);
+                BsonSerializer.RegisterSerializer(new TypeBsonSerializer());
+            }
+        }
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Type value)
         {
-            context.Writer.WriteStartDocument();
-            context.Writer.WriteName("@type");
-            context.Writer.WriteString(value.FullName);
-            context.Writer.WriteEndDocument();
+            if(value == null)
+            {
+                context.Writer.WriteNull();
+            }
+            else
+            {
+                context.Writer.WriteStartDocument();
+                context.Writer.WriteName("@type");
+                context.Writer.WriteString(value.FullName);
+                context.Writer.WriteEndDocument();
+            }
         }
 
         public override Type Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
-            context.Reader.ReadStartDocument();
-            var typeFullname = context.Reader.ReadString();
-            var type = Assembly.GetExecutingAssembly().GetType(typeFullname);
-            context.Reader.ReadEndDocument();
-            return type;
+            var bsonType = context.Reader.GetCurrentBsonType();
+            switch (bsonType)
+            {
+                case BsonType.Null:
+                    context.Reader.ReadNull();
+                    break;
+                case BsonType.Document:
+                    {
+                        context.Reader.ReadStartDocument();
+                        var typeFullname = context.Reader.ReadString();
+                        var type = Assembly.GetExecutingAssembly().GetType(typeFullname);
+                        context.Reader.ReadEndDocument();
+                        return type;
+                    }
+            }
+            return null;
         }
     }
 }
