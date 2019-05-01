4571e3342a7a8c0ee2fcf9a9cc75600c543e11fb
Jack Yum Cha <jackdatascience@gmail.com>
2019-05-01 16:17:36 +1000

    compact server update

---
+++ Jack.DataScience/MvcAngular.Generator.Lambda/CompactServerPayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcAngular.Generator.Lambda
+{
+    public class CompactServerPayload
+    {
+        public string Controller { get; set; }
+        public string Method { get; set; }
+        public string Credential { get; set; }
+        public List<object> Parameters { get; set; }
+    }
+}
