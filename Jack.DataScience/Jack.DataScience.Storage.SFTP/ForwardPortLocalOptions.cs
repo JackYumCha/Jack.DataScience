b22102a8c5d7a2f9f2dbc840d19d397733914400
Jack Yum Cha <jackdatascience@gmail.com>
2019-06-14 14:26:19 +1000

    add port forwarding support

---
+++ Jack.DataScience/Jack.DataScience.Storage.SFTP/ForwardPortLocalOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack.DataScience.Storage.SFTP
+{
+    public class ForwardPortLocalOptions
+    {
+        public List<LocalPortEntry> Ports { get; set; }
+    }
+}
---
+++ Jack.DataScience/Jack.DataScience.Storage.SFTP/LocalPortEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack.DataScience.Storage.SFTP
+{
+    public class LocalPortEntry
+    {
+        public string Local { get; set; }
+        public uint LocalPort { get; set; }
+        public string Remote { get; set; }
+        public uint RemotePort { get; set; }
+    }
+}
