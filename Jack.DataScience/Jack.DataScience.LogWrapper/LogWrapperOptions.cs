d9943bd368886df3bb70be3d60630f80cae63469
Zhenyu Shi <errisy@gmail.com>
2019-08-31 21:26:30 +1000

    log wrapper update & discrete math

--- Jack.DataScience/Jack.DataScience.LogWrapper/LogWrapperOptions.cs
+++ Jack.DataScience/Jack.DataScience.LogWrapper/LogWrapperOptions.cs
@@ -11,6 +11,9 @@ namespace Jack.DataScience.LogWrapper
         public string Database { get; set; }
         public SslProtocols SslProtocol { get; set; }
         public string CollectionName { get; set; }
+        public bool Capped { get; set; }
+        public int MaxDocuments { get; set; }
+        public long MaxSize { get; set; }
         public string Command { get; set; }
         public List<string> Arguments { get; set; }
     }
---
+++ Jack.DataScience/Jack.DataScience.ProcessExtensions/JsonLogMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack.DataScience.ProcessExtensions
+{
+    public class JsonLogMessage
+    {
+        public string JsonLogType { get; set; } = "[JsonLogType]";
+        public string Message { get; set; }
+        public string Json { get; set; }
+    }
+}
