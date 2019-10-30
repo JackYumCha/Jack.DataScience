a0a24482dfe74bc13ba0ede9e94294bf3167c1a7
Zhenyu Shi <zhenyu.shi@dominos.com.au>
2019-10-30 13:48:34 +1000

    Extensions

--- Jack.DataScience/Jack.DataScience.ConsoleExtensions/ConsoleExtensions.cs
+++ Jack.DataScience/Jack.DataScience.ConsoleExtensions/ConsoleExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+
 
 namespace Jack.DataScience.ConsoleExtensions
 {
@@ -39,6 +41,60 @@ namespace Jack.DataScience.ConsoleExtensions
             List<string> keys = new List<string>() { key };
             keys.AddRange(aliases);
             return args.Any(arg => keys.Any(k => k == arg));
+        }
+
+        public static int GetIntegerParameter(this string[] args, string key, int defaultValue, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            int result = defaultValue;
+            int.TryParse(value, out result);
+            return result;
+        }
+
+        public static int? GetNullableIntegerParameter(this string[] args, string key, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            int result;
+            if (int.TryParse(value, out result))
+                return result;
+            else
+                return null;
+        }
+
+        public static long GetLongParameter(this string[] args, string key, long defaultValue, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            long result = defaultValue;
+            long.TryParse(value, out result);
+            return result;
+        }
+
+        public static long? GetNullableLongParameter(this string[] args, string key, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            long result;
+            if (long.TryParse(value, out result))
+                return result;
+            else
+                return null;
+        }
+
+        public static DateTime GetDateTimeParameter(this string[] args, string key, DateTime defaultValue, string format, DateTimeStyles styles, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            DateTime result = defaultValue;
+            DateTime.TryParseExact(value, format, null, styles, out result);
+            return result;
+        }
+
+        public static DateTime? GetNullableDateTimeParameter(this string[] args, string key, string format, DateTimeStyles styles, params string[] aliases)
+        {
+            string value = args.GetParameter(key, aliases);
+            DateTime result;
+            if (DateTime.TryParseExact(value, format, null, styles, out result))
+                return result;
+            else
+                return null;
         }
     }
 }
