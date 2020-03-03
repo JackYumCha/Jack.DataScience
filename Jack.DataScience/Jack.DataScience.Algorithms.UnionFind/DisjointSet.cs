298084e96f79b3853506a1b5414903c897952ca9
Zhenyu Shi <zhenyu.shi@dominos.com.au>
2020-03-03 17:37:41 +1000

    dsu

---
+++ Jack.DataScience/Jack.DataScience.Algorithms.UnionFind/DisjointSet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Jack.DataScience.Algorithms.UnionFind
+{
+    public class DisjointSet
+    {
+        public int[] parent, rank;
+        public int count, size;
+        public DisjointSet(int size)
+        {
+            this.size = size;
+            parent = new int[size];
+            rank = new int[size];
+            count = size;
+            for (int i = 0; i < size; i++) parent[i] = i;
+        }
+
+        public int Find(int x)
+        {
+            if (x != parent[x]) parent[x] = Find(parent[x]);
+            return parent[x];
+        }
+
+        public bool Union(int x, int y)
+        {
+            if (x == y) return false;
+            int px = Find(x), py = Find(y);
+            if (px == py) return false;
+            if (rank[px] > rank[py]) parent[py] = px;
+            else if (rank[px] < rank[py]) parent[px] = py;
+            else
+            {
+                rank[px]++;
+                parent[py] = px;
+            }
+            count--;
+            return true;
+        }
+    }
+}
---
+++ Jack.DataScience/Jack.DataScience.Algorithms.UnionFind/DisjointSetExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jack.DataScience.Algorithms.UnionFind
+{
+    public static class DisjointSetExtensions
+    {
+        /// <summary>
+        /// remove the duplicates and return ordered array
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public static string[] MapUnionFindKeys(this IEnumerable<string> keys) => keys.Distinct().OrderBy(key => key).ToArray();
+        /// <summary>
+        /// remove the duplicates and return ordered array
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public static int[] MapUnionFindKeys(this IEnumerable<int> keys) => keys.Distinct().OrderBy(key => key).ToArray();
+        /// <summary>
+        /// remove the duplicates and return ordered array
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public static long[] MapUnionFindKeys(this IEnumerable<long> keys) => keys.Distinct().OrderBy(key => key).ToArray();
+        /// <summary>
+        /// build dictionary to allow mapping key to index with O(1) time
+        /// </summary>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static Dictionary<string, int> BuildDictionary(this string[] sets)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < sets.Length; i++) dict.Add(sets[i], i);
+            return dict;
+        }
+        /// <summary>
+        /// build dictionary to allow mapping key to index with O(1) time
+        /// </summary>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static Dictionary<long, int> BuildDictionary(this long[] sets)
+        {
+            Dictionary<long, int> dict = new Dictionary<long, int>();
+            for (int i = 0; i < sets.Length; i++) dict.Add(sets[i], i);
+            return dict;
+        }
+        /// <summary>
+        /// build dictionary to allow mapping key to index with O(1) time
+        /// </summary>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static Dictionary<int, int> BuildDictionary(this int[] sets)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < sets.Length; i++) dict.Add(sets[i], i);
+            return dict;
+        }
+        /// <summary>
+        /// map key to index with binary serach
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static int UnionFindIndex(this string key, string[] sets) => Array.BinarySearch(sets, key);
+        /// <summary>
+        /// map key to index with binary serach
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static int UnionFindIndex(this long key, long[] sets) => Array.BinarySearch(sets, key);
+        /// <summary>
+        /// map key to index with binary serach
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sets"></param>
+        /// <returns></returns>
+        public static int UnionFindIndex(this int key, int[] sets) => Array.BinarySearch(sets, key);
+
+        /// <summary>
+        /// get each subgraph as an int[]
+        /// </summary>
+        /// <param name="disjointSet"></param>
+        /// <returns></returns>
+        public static IEnumerable<int[]> Subgraphs(this DisjointSet disjointSet)
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+            for(int i = 0; i < disjointSet.size; i++)
+            {
+                int p = disjointSet.Find(i);
+                if (dict.ContainsKey(p))
+                {
+                    dict[p].Add(i);
+                }
+                else
+                {
+                    List<int> list = new List<int>() { i };
+                    dict.Add(p, list);
+                }
+            }
+            return dict.Values.Select(list => list.OrderBy(i => i).ToArray());
+        }
+    }
+}
